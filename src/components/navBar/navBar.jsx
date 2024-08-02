f884cd9702ffcb096d8dc221fba175ac9713306b
WIN-20230818KUB\OWNER <manchinghin@gmail.com>
2024-08-01 20:43:23 -0400

    1. modify logout btn, text become login when user not logged in 2. git ignore build folder "dist"

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,8 +1,14 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 export const NavBar = ({onLoggedOut}) => {
+    var user = JSON.parse(localStorage.getItem("user"));
+
     const logoutBtn = <Button
     onClick={ev => onLoggedOut()} >Logout</Button>
+
+    const loginBtn = <Nav.Link href="/login">
+        <Button>Login</Button>
+    </Nav.Link>
 
     return <Navbar bg="light" expand="lg">
         <Container>
@@ -14,7 +20,7 @@ export const NavBar = ({onLoggedOut}) => {
                 <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
             </Navbar.Collapse>
-            {logoutBtn}
+            {user ? logoutBtn : loginBtn}
         </Container>
     </Navbar>
 }
